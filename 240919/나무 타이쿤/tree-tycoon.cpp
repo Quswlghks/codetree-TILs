c98ab9affabef4e12ee5653a49cc1d508c8807bc
변지환 - Byeon JiHwan <99013865+Quswlghks@users.noreply.github.com>
2024-09-19 00:39:32 +0900

    Title: 나무 타이쿤, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/나무 타이쿤/tree-tycoon.cpp
+++ 240919/나무 타이쿤/tree-tycoon.cpp
@@ -1,101 +1,88 @@
 #include <iostream>
+#include <vector>
+#include <set>
 using namespace std;
 
-int n,m;
+int n, m;
 int Board[15][15];
-bool tonicBoard[15][15] = {false};
+vector<pair<int,int>> medi;
+set<pair<int,int>> visited;
 pair<int,int> moveInfo[100];
 
 int dx[8] = {0,-1,-1,-1,0,1,1,1};
 int dy[8] = {1,1,0,-1,-1,-1,0,1};
 
-int dxMore[4]={-1,-1,1,1};
-int dyMore[4]={1,-1,1,-1};
+int dxMore[4]={1,1,-1,-1};
+int dyMore[4]={1,-1,-1,1};
 
 void Input(){
     cin >> n >> m;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
             cin >> Board[i][j];
         }
     }
-    // 초기 특수 영양제 위치 설정
-    tonicBoard[n-2][0] = true;
-    tonicBoard[n-2][1] = true;
-    tonicBoard[n-1][0] = true;
-    tonicBoard[n-1][1] = true;
-
-    for(int i=0;i<m;i++) cin >> moveInfo[i].first >> moveInfo[i].second;
+    medi = {{n-1,0}, {n-2,0}, {n-1,1}, {n-2,1}};
+    for(int i=0; i<m; i++) cin >> moveInfo[i].first >> moveInfo[i].second;
 }
 
-void moveTonic(int year){
+void moveMedi(int year){
     int d = moveInfo[year].first-1; int p = moveInfo[year].second;
-
-    bool tonicBoardResult[15][15] = {false};
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(tonicBoard[i][j]){
-                // 수정: 음수 처리를 위해 n을 더하고 모듈러 연산
-                int nx = (i + dx[d] * p % n + n) % n;
-                int ny = (j + dy[d] * p % n + n) % n;
-                tonicBoardResult[nx][ny]=true;
-            }
-        }
-    }
-    // 결과를 원래 배열로 복사
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            tonicBoard[i][j] = tonicBoardResult[i][j];
-        }
+    for(auto& pos : medi){
+        pos.first = (pos.first + dx[d] * p + n * 1000) % n;
+        pos.second = (pos.second + dy[d] * p + n * 1000) % n;
     }
 }
 
-void applyTonic(){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(tonicBoard[i][j]){
-                // 기본 성장
-                Board[i][j]++;
-                
-                // 대각선 방향 추가 성장 계산
-                int moreGrow = 0;
-                for(int z=0;z<4;z++){
-                    int nx = i+dxMore[z]; int ny = j+dyMore[z];
-                    if(nx<0||nx>=n||ny<0||ny>=n) continue;
-                    if(Board[nx][ny]>=1) moreGrow++;
-                }
-                Board[i][j] += moreGrow;
+void grow(){
+    visited.clear();
+    for(auto& pos : medi){
+        Board[pos.first][pos.second]++;
+        visited.insert(pos);
+    }
+    
+    vector<int> growAmount(medi.size(), 0);
+    for(int i = 0; i < medi.size(); i++){
+        int r = medi[i].first, c = medi[i].second;
+        for(int j = 0; j < 4; j++){
+            int nr = r + dxMore[j], nc = c + dyMore[j];
+            if(nr >= 0 && nr < n && nc >= 0 && nc < n && Board[nr][nc] > 0){
+                growAmount[i]++;
             }
         }
+    }
+    
+    for(int i = 0; i < medi.size(); i++){
+        Board[medi[i].first][medi[i].second] += growAmount[i];
     }
 }
 
-void buyTonic(){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            // 수정: 이전에 영양제를 받지 않은 위치만 고려
-            if(!tonicBoard[i][j] && Board[i][j]>=2){
-                Board[i][j]-=2;
-                tonicBoard[i][j]=true;
+void makeNewMedi(){
+    vector<pair<int,int>> newMedi;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(Board[i][j] >= 2 && visited.find({i,j}) == visited.end()){
+                Board[i][j] -= 2;
+                newMedi.push_back({i,j});
             }
         }
     }
-    tonicBoard[15][15] = {false};
+    medi = newMedi;
 }
 
 void Solve(){
     for(int i=0; i<m; i++){
-        moveTonic(i);
-        applyTonic();
-        buyTonic();
+        moveMedi(i);
+        grow();
+        makeNewMedi();
     }
     int result = 0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            result+=Board[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            result += Board[i][j];
         }
     }
-    cout<<result;
+    cout << result;
 }
 
 int main() {

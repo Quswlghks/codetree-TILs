1370683744144e67ca39b0255e9b0ae4a50caef7
변지환 - Byeon JiHwan <99013865+Quswlghks@users.noreply.github.com>
2024-10-07 14:02:39 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241007/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int Map[100][100]; int result;
+
+int main() {
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> Map[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        int count = 1;
+        for(int j=1;j<n;j++){
+            if(Map[i][j]==Map[i][j-1]){ count++; if(count==m) {result++; break;}}
+            else count = 1;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        int count = 1;
+        for(int j=1;j<n;j++){
+            if(Map[j][i]==Map[j-1][i]){ count++; if(count==m) {result++; break;}}
+            else count = 1;
+        }
+    }
+    cout << result;
+    return 0;
+}
